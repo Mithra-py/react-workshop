b16e7ade642d1bef888dcd42fe35a8ac04d92c2e
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-12-14 15:16:05 +0300

    language card

---
+++ language-card/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import headimg from '../../assets/react.svg';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='headerimg'>
+      <img src={headimg} alt="" />
+    </div>
+  )
+}
+
+export default Header
