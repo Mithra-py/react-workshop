1632af5587ea4802fa890bcf6b64b16bc4faf683
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-29 16:27:47 +0300

    starter and navbar

---
+++ tour-travel-project/src/App.js
@@ -0,0 +1,15 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
