c1589b0825ca63ebef41ca88a11f5b52d9a51a83
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-30 13:40:14 +0300

    builded router

--- tour-travel-project/src/App.js
+++ tour-travel-project/src/App.js
@@ -1,13 +1,22 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-
 import React from 'react'
+import Home from './routes/Home';
+import About from './routes/About';
+import Service from './routes/Service';
+import Contact from './routes/Contact';
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/service' element={<Service/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   )
 }
