3f335a526fab96ed490aa545521e5b88fa458c08
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-30 16:18:52 +0300

    destination complete

--- tour-travel-project/src/components/Destination.jsx
+++ tour-travel-project/src/components/Destination.jsx
@@ -4,6 +4,7 @@ import img1 from "../assets/1.jpg"
 import img2 from "../assets/2.jpg"
 import img3 from "../assets/3.jpg"
 import img4 from "../assets/4.jpg"
+import DestinationData from './DestinationData'
 
 const Destination = () => {
     return (
@@ -11,16 +12,27 @@ const Destination = () => {
             <h1>Popular Destinations</h1>
             <p>Tours give you the opportunity to see a lot, within a time frame.</p>
 
-            <div className='first-des'>
-                <div className='des-text'>
-                    <h2>Taal Volcano, Batangas</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim asperiores ullam animi, ducimus consequatur voluptatibus quaerat debitis nemo repudiandae dicta a maxime mollitia cupiditate similique repellendus sint officia illum voluptatum vel, autem hic expedita? Accusantium possimus iste minima porro dignissimos, odit in repellendus incidunt ipsa hic reprehenderit odio sapiente quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consectetur, necessitatibus ratione sit deleniti iusto in accusamus repudiandae tempore corporis!</p>
-                </div>
-                <div className='image'>
-                <img src={img1} alt="dest-img" />
-                <img src={img2} alt="dest-img" />
-                </div>
-            </div>
+            <DestinationData
+
+                className="first-des"
+                heading="Taal Volcano, Batangas"
+
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim asperiores ullam animi, ducimus consequatur voluptatibus quaerat debitis nemo repudiandae dicta a maxime mollitia cupiditate similique repellendus sint officia illum voluptatum vel, autem hic expedita? Accusantium possimus iste minima porro dignissimos, odit in repellendus incidunt ipsa hic reprehenderit odio sapiente quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consectetur, necessitatibus ratione sit deleniti iusto in accusamus repudiandae tempore corporis!"
+
+                img1={img1}
+                img2={img2}
+            />
+
+            <DestinationData
+
+                className="first-des-reverse"
+                heading="Mt. Daauldul, Batanaas"
+
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim asperiores ullam animi, ducimus consequatur voluptatibus quaerat debitis nemo repudiandae dicta a maxime mollitia cupiditate similique repellendus sint officia illum voluptatum vel, autem hic expedita? Accusantium possimus iste minima porro dignissimos, odit in repellendus incidunt ipsa hic reprehenderit odio sapiente quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consectetur, necessitatibus ratione sit deleniti iusto in accusamus repudiandae tempore corporis!"
+
+                img1={img3}
+                img2={img4}
+            />
         </div>
     )
 }
