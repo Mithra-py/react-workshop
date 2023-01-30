28d24036d46d1853dd73c3cdb096d6d04bf91bf4
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-30 14:11:34 +0300

    hero components

--- tour-travel-project/src/routes/Home.jsx
+++ tour-travel-project/src/routes/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import img12 from "../assets/12.jpg"
 
 const Home = () => {
   return (
     <>
-    <h1>This is Home</h1>
+    <Hero
+    cName="hero"
+    heroImg={img12}
+    title="Your Journey Your Story"
+    text="Choose Your Favourite Destination."
+    buttonText="Travel Plan"
+    url="/"
+    btnClass="show"
+    />
     </>
   )
 }
