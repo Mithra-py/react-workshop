60f210f304a5bbb3432c3e210d11e3a2e7f8d3b3
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-31 14:30:54 +0300

    trip done

--- tour-travel-project/src/components/TripData.jsx
+++ tour-travel-project/src/components/TripData.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const TripData = () => {
+const TripData = (props) => {
   return (
     <div className='t-card'>
-        
+        <div className='t-image'>
+            <img src={props.image} alt="image" />
+        </div>
+        <h4>{props.heading}</h4>
+        <p>{props.text}</p>
     </div>
   )
 }
