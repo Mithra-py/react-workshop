8d75bbd39af75787a30bb5a2ec5783806c02dd16
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-02-02 16:21:52 +0300

    header

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+     <Header/> 
+    </>
   );
 }
 
