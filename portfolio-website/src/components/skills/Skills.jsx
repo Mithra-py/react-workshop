fd0ace5d34b152040b57c6994f5d7c2b9f5cfc8e
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-02-03 17:06:42 +0300

    skills starter

---
+++ portfolio-website/src/components/skills/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Frontend from './Frontend'
+import "./skills.css"
+
+const Skills = () => {
+  return (
+    <section className="skills" id="section">
+        <h2 className="section__title">Skills</h2>
+        <span className="section__subtitle">My tecnical level</span>
+
+        <div className="skill__container container grid">
+            <Frontend/>
+        </div>
+    </section>
+  )
+}
+
+export default Skills
