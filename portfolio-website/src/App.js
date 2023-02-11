562a8afedceabe2c333838e57dc9d616ed7c2452
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-02-11 17:50:36 +0300

    footer starter

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
@@ -22,6 +23,7 @@ function App() {
       <Testimonials/>
       <Contact/>
      </main>
+     <Footer/>
     </>
   );
 }
