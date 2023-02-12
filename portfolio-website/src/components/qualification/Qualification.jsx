8abe4408da852b716066b63f46a4e6084fc6f45e
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-02-12 15:43:43 +0300

    qualification bug fix 2

--- portfolio-website/src/components/qualification/Qualification.jsx
+++ portfolio-website/src/components/qualification/Qualification.jsx
@@ -140,11 +140,7 @@ const Qualification = () => {
                                 </div>
                             </div>
 
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-
-                            </div>
+                            
                         </div>
 
 
