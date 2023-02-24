5d3364488afb8cc54c8f1982ef7b14be1f7617bc
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-02-24 20:43:06 +0300

    card component box shadow

--- eshop-ecommerce-app/src/pages/auth/Login.js
+++ eshop-ecommerce-app/src/pages/auth/Login.js
@@ -3,6 +3,7 @@ import styles from "./auth.module.scss"
 import loginImg from "../../assets/login.png"
 import { Link } from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa"
+import Card from '../../components/card/Card'
 
 const Login = () => {
     return (
@@ -11,24 +12,27 @@ const Login = () => {
                 <img src={loginImg} alt="login" width="400" />
             </div>
 
-            <div className={styles.form}>
-                <h2>Login</h2>
-                <form>
-                    <input type="text" placeholder='Email' required />
-                    <input type="password" placeholder='Password' required />
-                    <button className='--btn --btn-primary --btn-block'>Login</button>
-                    <div className={styles.links}>
-                        <Link to="/reset">Reset Password</Link>
-                    </div>
-                    <p>-- or --</p>
-                </form>
+            <Card>
+                <div className={styles.form}>
+                    <h2>Login</h2>
 
-                <button className='--btn --btn-danger --btn-block'><FaGoogle color="#fff" /> Login With Google</button>
-                <span className={styles.register}>
-                    <p>Don't have an account?</p>
-                    <Link to="/Register">Register</Link>
-                </span>
-            </div>
+                    <form>
+                        <input type="text" placeholder='Email' required />
+                        <input type="password" placeholder='Password' required />
+                        <button className='--btn --btn-primary --btn-block'>Login</button>
+                        <div className={styles.links}>
+                            <Link to="/reset">Reset Password</Link>
+                        </div>
+                        <p>-- or --</p>
+                    </form>
+
+                    <button className='--btn --btn-danger --btn-block'><FaGoogle color="#fff" /> Login With Google</button>
+                    <span className={styles.register}>
+                        <p>Don't have an account?</p>
+                        <Link to="/Register">Register</Link>
+                    </span>
+                </div>
+            </Card>
         </section>
     )
 }
